a46f75185b2eb9a99dc056214e6154bbc4f5b96f
Patrick Perini <pcperini@gmail.com>
2018-08-18 08:28:21 -0700

    cleans up a didSet

--- Tusk/Views/ImageView.swift
+++ Tusk/Views/ImageView.swift
@@ -10,22 +10,18 @@ import UIKit
 import AlamofireImage
 
 @IBDesignable class ImageView: UIImageView {
-    @IBInspectable var round: Bool = false {
-        didSet {
-            self.clipsToBounds = self.round
-            if (self.round != oldValue) {
-                self.setNeedsLayout()
-            }
-        }
-    }
+    @IBInspectable var round: Bool = false { didSet { self.setNeedsLayout() } }
     
     @IBInspectable var borderWidth: CGFloat = 1.0 { didSet { self.setNeedsLayout() } }
     @IBInspectable var borderColor: UIColor = .clear { didSet { self.setNeedsLayout() } }
-    
+
     override func layoutSubviews() {
+        self.clipsToBounds = self.round
         self.layer.cornerRadius = self.round ? (self.layer.bounds.width / 2.0) : 0
+        
         self.layer.borderColor = self.borderColor.cgColor
         self.layer.borderWidth = self.borderWidth
+        
         super.layoutIfNeeded()
     }
 }
