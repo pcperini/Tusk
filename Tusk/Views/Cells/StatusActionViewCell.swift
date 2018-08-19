eb8dd2e0ae518fe388aa187337562402318ff54c
Patrick Perini <pcperini@gmail.com>
2018-08-19 16:00:52 -0700

    adds status action tray

---
+++ Tusk/Views/Cells/StatusActionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  StatusActionViewCell.swift
+//  Tusk
+//
+//  Created by Patrick Perini on 8/19/18.
+//  Copyright © 2018 Patrick Perini. All rights reserved.
+//
+
+import UIKit
+
+class StatusActionViewCell: UITableViewCell {
+    
+}
