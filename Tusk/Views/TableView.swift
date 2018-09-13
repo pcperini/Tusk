3d302e5280c6c07b9f31e7f326eac30ed74ecd4e
Patrick Perini <pcperini@gmail.com>
2018-09-12 20:14:02 -0700

    adds batch update queueing

---
+++ Tusk/Views/TableView.swift
@@ -0,0 +1,33 @@
+//
+//  TableView.swift
+//  Tusk
+//
+//  Created by Patrick Perini on 9/12/18.
+//  Copyright © 2018 Patrick Perini. All rights reserved.
+//
+
+import UIKit
+
+class TableView: UITableView {
+    private var updateQueue: [((() -> Void), ((Bool) -> Void)?)] = [] {
+        didSet {
+            if oldValue.isEmpty {
+                self.performNextBatchUpdates()
+            }
+        }
+    }
+    
+    override func performBatchUpdates(_ updates: (() -> Void)?, completion: ((Bool) -> Void)? = nil) {
+        self.updateQueue.append(({ updates?() }, completion))
+    }
+ 
+    private func performNextBatchUpdates() {
+        guard !self.updateQueue.isEmpty else { return }
+        
+        let next = self.updateQueue.removeFirst()
+        super.performBatchUpdates(next.0) { (completed) in
+            next.1?(completed)
+            self.performNextBatchUpdates()
+        }
+    }
+}
